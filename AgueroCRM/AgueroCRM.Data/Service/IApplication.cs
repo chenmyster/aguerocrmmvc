f0d9e4f71b8f8ae2f49967d3c68b82a8bf096aee
Sitong Chen <s.chen329@gmail.com>
2019-10-13 11:49:29 +0100

    Service Interfaces

---
+++ AgueroCRM/AgueroCRM.Data/Service/IApplication.cs
@@ -0,0 +1,18 @@
+﻿using AgueroCRM.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgueroCRM.Data.Service
+{
+    public interface IApplication
+    {
+        IEnumerable<Application> GetAll();
+        Application Get(int id);
+        void Add(Application application);
+        void Update(Application application);
+        void Delete(int id);
+    }
+}
---
+++ AgueroCRM/AgueroCRM.Data/Service/ICustomerContract.cs
@@ -0,0 +1,18 @@
+﻿using AgueroCRM.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgueroCRM.Data.Service
+{
+    public interface ICustomerContract
+    {
+        IEnumerable<CustomerContract> GetAll();
+        CustomerContract Get(int id);
+        void Add(CustomerContract customerContract);
+        void Update(CustomerContract customerContract);
+        void Delete(int id);
+    }
+}
